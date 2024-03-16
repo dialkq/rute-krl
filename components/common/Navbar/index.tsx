88ff863ed50828dcab689d800e9d64669c8554eb
dialkq <dialkq@gmail.com>
2024-03-16 13:20:00 +0700

    update style navbar

--- components/common/Navbar/index.tsx
+++ components/common/Navbar/index.tsx
@@ -7,15 +7,25 @@ import { Input } from "@/components/ui/input";
 const Navbar = () => {
   return (
     <div className="w-full mx-auto flex flex-col md:flex-row ">
-      <div className="w-full flex items-center py-2 md:py-4 justify-center md:justify-normal">
-        <SiTestrail className="text-xl text-slate-400 hover:text-black mr-1" />
-        <p className="lg:text-xl text-slate-400 text-center font-semibold font-mono">
-          rute-krl
-        </p>
+      <div className="w-full flex items-center py-2 md:py-4 justify-between md:justify-normal">
+        <div className="flex">
+          <SiTestrail className="text-xl text-slate-400 my-auto hover:text-black mr-1" />
+          <p className="lg:text-xl text-slate-400 text-center font-semibold font-mono">
+            rute-krl
+          </p>
+        </div>
+        <div className="flex md:hidden">
+          <ThemeBtn />
+        </div>
       </div>
       <div className="flex justify-between items-center md:py-4">
-        <Input placeholder="Cari Stasiun" className="placeholder:text-slate-400 md:mr-5 rounded-lg font-mono border-2 border-slate-200 focus:border-none w-40" />
-        <ThemeBtn />
+        <Input
+          placeholder="Cari Stasiun"
+          className="placeholder:text-slate-400 md:mr-3 rounded-lg font-mono border-2 border-slate-200 focus:border-none w-full md:w-52 lg:w-60"
+        />
+        <div className="hidden md:flex">
+          <ThemeBtn />
+        </div>
       </div>
     </div>
   );
--- components/common/ThemeBtn/index.tsx
+++ components/common/ThemeBtn/index.tsx
@@ -17,7 +17,7 @@ export function ThemeBtn() {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger className="p-2" asChild>
+      <DropdownMenuTrigger className="" asChild>
         <Button variant="ghost" size="icon">
           <Sun className="hover:text-black h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
