7edd3ced0e72f177735ce502b871b4bda58b24e5
dialkq <dialkq@gmail.com>
2024-03-27 22:58:50 +0700

    create api local

--- app/components/common/CardStation/index.tsx
+++ app/components/common/CardStation/index.tsx
@@ -1,35 +1,9 @@
 "use client";
 import { MdOutlineKeyboardArrowUp } from "react-icons/md";
 import CardRute from "../CardRute";
-import { useQuery } from 'react-query';
-import axios from 'axios';
 import React from "react";
 
 const CardStation = () => {
-  const apiUrl = 'https://www.api.comuline.com/v1/schedule/AC';
-  const { data, isLoading, isError } = useQuery('stationData', async () => {
-    try {
-      const response = await axios.get(apiUrl);
-      return response.data;
-    } catch (error) {
-      throw new Error('Error fetching data from API');
-    }
-  });
-  
-  React.useEffect(() => {
-    if (data) {
-      console.log('Data from API:', data);
-    }
-  }, [data]);
-
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
-
-  if (isError) {
-    return <p>Error fetching data from API</p>;
-  }
-
   return (
     <div className="w-full flex flex-col justify-between mt-5 mb-10 md:mt-5 md:mb-16">
       {/* STATION TITLE */}
